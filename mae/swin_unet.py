0e9abdbbdfc1066c6bb1ee58eb7dd3c281481803
Bin Yang <ybMaschinenbau@gmail.com>
2023-08-05 22:03:05 +0200

    swin transformer v2

--- mae/swin_unet.py
+++ mae/swin_unet.py
@@ -9,6 +9,7 @@ from functools import partial
 from util.filter import *
 
 from util.evaluation import inverse_huber_loss
+from swin_transformer_v2 import SwinTransformerBlockV2
 
 class DropPath(nn.Module):
     def __init__(self, drop_prob: float = 0.):
@@ -278,35 +279,6 @@ class SwinTransformerBlock(nn.Module):
         x = x + x_copy
         return x
     
-
-class SwinTransformerBlockV2(nn.Module):
-    def __init__(self, dim, num_heads, window_size=7, shift=False, mlp_ratio=4., qkv_bias=True,
-                 drop=0., attn_drop=0., drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
-        super().__init__()
-        self.norm1 = norm_layer(dim)
-        self.attn = WindowAttention(dim, window_size=window_size, num_heads=num_heads, qkv_bias=qkv_bias,
-                                    attn_drop=attn_drop, proj_drop=drop, shift=shift)
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        self.norm2 = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-
-    def forward(self, x):
-        x_copy = x
-        x = self.norm1(x)
-
-        x = self.attn(x)
-        x = self.drop_path(x)
-        x = x + x_copy
-
-        x_copy = x
-        x = self.norm2(x)
-
-        x = self.mlp(x)
-        x = self.drop_path(x)
-        x = x + x_copy
-        return x
-
 
 class BasicBlock(nn.Module):
     def __init__(self, index: int, embed_dim: int = 96, window_size: int = 7, depths: tuple = (2, 2, 6, 2),
